28fbe115f01453ff03407123ae5633916c080fb2
Khiem Nguyen <khiemalbavds@gmail.com>
2020-09-27 22:53:18 +0700

    Giao dien quizz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuizzComponent } from './quizz/quizz.component';
 import { QuizzItemComponent } from './quizz/quizz-item/quizz-item.component';
+import { QuizzHoverDirective } from './quizz/quizz-item/quizz-hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizzComponent,
     QuizzItemComponent,
+    QuizzHoverDirective,
   ],
   imports: [
     BrowserModule
---
+++ src/app/quizz/quizz-item/quizz-hover.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, Host, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appQuizzHover]'
+})
+export class QuizzHoverDirective {
+
+  @HostBinding('class.active') isHover: boolean = false;
+
+  @HostListener('mouseover') onMouseOver() {
+    this.isHover = true;
+  }
+
+  @HostListener('mouseout') onMouseout() {
+    this.isHover = false;
+  }
+
+  constructor() { }
+
+}
